071e24a7b6ab0184a67a49eb93c397caa6199a18
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-18 00:16:42 +0500

    Add TestHelper to separate button send actions from test suit

---
+++ ButtonTapTests/TestHelpers.swift
@@ -0,0 +1,12 @@
+//
+//  TestHelpers.swift
+//  ButtonTapTests
+//
+//  Created by HAQQQABD on 18/08/2023.
+//
+
+import UIKit
+
+func tap(_ button: UIButton){
+    button.sendActions(for: .touchUpInside)
+}
