cdcb5e6e1a7336708846ca5d319b5ad2f5fcfdaf
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-17 23:36:05 +0500

    Make getter available and remains setter as private

--- ButtonTap/ViewController.swift
+++ ButtonTap/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet private var button: UIButton!
+    @IBOutlet private(set) var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
