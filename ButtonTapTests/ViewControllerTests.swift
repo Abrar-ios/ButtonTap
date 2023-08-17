3a6c1b23bd07c882f2e50ac6442f12fa692c0248
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-17 23:33:37 +0500

    Add ViewControllerTests suit
    .Implemente test_zero to confirm communication between test suit and production code

---
+++ ButtonTapTests/ViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  ViewControllerTests.swift
+//  ButtonTapTests
+//
+//  Created by HAQQQABD on 17/08/2023.
+//
+
+import XCTest
+@testable import ButtonTap
+
+final class ViewControllerTest: XCTestCase {
+    func test_zero(){
+        XCTFail("There is no tests are implemneted in ViewControllerTests yet")
+    }
+}
